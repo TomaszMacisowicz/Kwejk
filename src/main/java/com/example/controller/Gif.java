cd1e7c38bff546bc49f687c6d5ed3658513436e1
Tomasz Macisowicz <tmacisowicz@gmail.com>
2017-03-31 16:30:36 +0200

    add new class

---
+++ src/main/java/com/example/controller/Gif.java
@@ -0,0 +1,7 @@
+package com.example.controller;
+
+/**
+ * Created by Macu on 2017-03-31.
+ */
+public class Gif {
+}
--- src/main/java/com/example/controller/GifController.java
+++ src/main/java/com/example/controller/GifController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Macu on 2017-03-31.
@@ -13,4 +14,11 @@ public class GifController {
 public String listGifs(){
         return "home";
     }
+    @PostMapping
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", "michalos", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
 }
